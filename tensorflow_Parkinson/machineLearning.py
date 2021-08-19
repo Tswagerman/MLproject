8b267166d262c858b2fc2c51589f9a8f01187ea1
Tswagerman <thomasswagerman@hotmail.com>
2021-08-19 11:36:32 +0200

    save

--- tensorflow_Parkinson/machineLearning.py
+++ tensorflow_Parkinson/machineLearning.py
@@ -7,29 +7,27 @@ from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
 from keras.models import load_model
 
-#Global variable, used outside '__calcAccuracy' method
-accuracy = 0
-
 class model:
     def __init__(self, parkinson_training_features, parkinson_training_labels, parkinson_testing_features, parkinson_testing_labels):  
         self.prediction_array = []
         self.number_models_saved = 0
+        self.__nrModelsSaved()
+        #Stored some of the best performing models,
+        #the average accuracy is about 85% over the testing data. These models differ in tuning parameters
         #Boolean to use '.hdf5' models that are stored in 'cwd\models'
         self.use_saved_models = self.__yesOrNo()
+        #self.use_saved_models = False
         #Tuning parameters
         #ExponentialDecay = initial_learning_rate * decay_rate ^ (step / decay_steps)
         self.epochs = 500     #500  | Epoch: one forward pass and one backward pass of all the training examples
-        self.LR = 0.4         #0.45 | Learning rate
-        self.DS = 100         #50   | Decay steps
-        self.DR = 0.96        #0.98 | Decay rate
-        self.model_runs = 15  #15   | Number of models being used to form the committee machine
-        self.batch_size = 300 #55   | Batch size: number of training examples in one epoch      
+        self.LR = 0.1        #0.45 | Learning rate
+        self.DS = 10         #50   | Decay steps
+        self.DR = 0.9        #0.98 | Decay rate
+        self.model_runs = 11  #15   | Number of models being used to form the committee machine
+        self.batch_size = 55 #55   | Batch size: number of training examples in one epoch  
         self.__modelRuns(parkinson_training_features, parkinson_training_labels, parkinson_testing_features, parkinson_testing_labels)
        
     def __modelRuns(self, parkinson_training_features, parkinson_training_labels, parkinson_testing_features, parkinson_testing_labels):
-        #Stored some of the best performing models,
-        #the average accuracy is about 85% over the testing data. These models differ in tuning parameters
-        self.__nrModelsSaved()
         #Averaging over a number of models. Committee machine
         for modelnr in range(self.model_runs): 
             if (self.use_saved_models == True):
@@ -43,10 +41,11 @@ class model:
             self.prediction_array.append(new_prediction_array)
             predictArray = np.asarray(new_prediction_array) 
             self.__calcAccuracy(predictArray, parkinson_testing_labels, "Mid-run testing")
-        tupleArray = tuple(map(mean, zip(*self.prediction_array)))
-        predictArray = np.asarray(tupleArray) 
+        tuple_array = tuple(map(mean, zip(*self.prediction_array)))
+        predict_array = np.asarray(tuple_array) 
+        print(predict_array)
         #The average testing data is used to validate the accuracy of the model.
-        self.__calcAccuracy(predictArray, parkinson_testing_labels, "Testing Data ")
+        self.__calcAccuracy(predict_array, parkinson_testing_labels, "Testing Data ")
 
     def __createAndTrainModel(self, parkinson_training_features, parkinson_training_labels):
         features_shape = parkinson_training_features.shape[1:]
@@ -58,11 +57,11 @@ class model:
         #Create model 
         input = keras.Input(shape=features_shape) #10 features, creating (10,) Tensor
         features = normalizer(input)
-        hidden_layer_1 = layers.Dense(units = 10, activation="tanh") #Weights 1 * 10 + 10 * 10
+        hidden_layer_1 = layers.Dense(units = 5, activation="tanh") #Weights 1 * 5 + 5 * 5
         output_tensor_1 = hidden_layer_1(features)
-        hidden_layer_2 = layers.Dense(units = 10, activation="tanh") #Weights 10 * 10 + 10 * 1
+        hidden_layer_2 = layers.Dense(units = 5, activation="tanh") #Weights 5 * 5 + 5 * 1
         output_tensor_2 = hidden_layer_2(output_tensor_1)
-        output_layer = layers.Dense(units = 1, activation="sigmoid") #Weights 10 * 1 + 1
+        output_layer = layers.Dense(units = 1, activation="sigmoid") #Weights 5 * 1 + 1
         output_tensor_3 = output_layer(output_tensor_2)
         model = keras.Model(input, output_tensor_3)
 
@@ -99,9 +98,8 @@ class model:
             if (prediction_array_rounded[iteration] == label_array[iteration]): #Prediction correct
                 accuracy_counter += 1
             else: #Prediction incorrect
-                pass
-                #print("iteration = ", iteration, " prediction: ", prediction_array_rounded[iteration], " Label: ", label_array[iteration], "PreRounding:", prediction_array[iteration])
-        global accuracy 
+                #pass
+                print("iteration = ", iteration, " prediction: ", prediction_array_rounded[iteration], " Label: ", label_array[iteration], "PreRounding:", prediction_array[iteration])
         accuracy = (accuracy_counter / label_array.size) * 100 
         print("############ Accuracy =", "{:.2f}".format(accuracy), "% ###########")
 
